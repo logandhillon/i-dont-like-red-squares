c28b74bd0098d1dd90615ff7f6776cad9c650f59
Logan Dhillon <logandhi64@gmail.com>
2024-02-16 14:14:08 -0500

    Make entity abstract class

--- game/entities/entity.py
+++ game/entities/entity.py
@@ -1,8 +1,10 @@
 from game.entity_groups import ALL_ENTITIES
 import pygame.sprite
+from game.globals import Display
+from abc import ABC, abstractmethod
 
 
-class Entity(pygame.sprite.Sprite):
+class Entity(pygame.sprite.Sprite, ABC):
     def __init__(self, size: int, speed: int, color: pygame.color):
         super(Entity, self).__init__()
         self.surf = pygame.Surface((size, size))
@@ -10,3 +12,7 @@ class Entity(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect()
         self.speed = speed
         ALL_ENTITIES.add(self)
+
+    @abstractmethod
+    def update(self):
+        pass
